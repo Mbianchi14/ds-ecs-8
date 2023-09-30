a8a65b32439370e33090830dda2900cdf20d9372
Mbianchi14 <bianchimauricio14@gmail.com>
2023-09-29 21:49:56 -0300

    commit ecs-8

---
+++ db-ecs-8.sql
@@ -0,0 +1,40 @@
+/*
+SQLyog Community v13.2.0 (64 bit)
+MySQL - 10.4.28-MariaDB : Database - ecs-8
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`ecs-8` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+
+USE `ecs-8`;
+
+/*Table structure for table `autos` */
+
+DROP TABLE IF EXISTS `autos`;
+
+CREATE TABLE `autos` (
+  `Id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `Marca` text NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `autos` */
+
+insert  into `autos`(`Id`,`Marca`) values 
+(1,'Renault'),
+(2,'Ford'),
+(3,'Fiat'),
+(4,'Mercedez Benz');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
